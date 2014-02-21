f1faaf047f5299f7effcc471cb2ce279510bf90b
Benjamin Paul Dunphy & Spyder Monkee McHenry <student@epicodus.com>
2014-02-20 17:40:30 -0800

    Working code of fibonacci recursion

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,21 @@ var factorial = function(number) {
   };
 };
 
+
+var fibonacci = function(number) {
+  if(number === 1){
+    return 0;
+  } else if(number === 2){
+      return 1;
+  } else if(number === 3){
+    return 1;
+  } else {
+    return fibonacci(number-1)+fibonacci(number-2);
+  }
+};
+
+
+
 $(function() {
   $('form#factorial').submit(function(event) {
     var number = $('#input-number').val();
--- spec/specs.js
+++ spec/specs.js
@@ -12,3 +12,18 @@ describe('factorial', function() {
     factorial(5).should.equal(120);
   });
 });
+describe('fibonacci', function() {
+  it('returns a 0 when you enter 1', function () {
+    fibonacci(1).should.equal(0);
+  });
+  it('returns a 1 when you enter 2', function () {
+    fibonacci(2).should.equal(1);
+  });
+  it('returns a 2 when you enter 3', function () {
+    fibonacci(3).should.equal(1);
+  });
+
+  it('should sum the first two numbers of the array', function () {
+    fibonacci(12).should.equal(89);
+  });  
+});
